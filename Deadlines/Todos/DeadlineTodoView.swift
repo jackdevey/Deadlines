da137beabf283d039d6cae319b042c2e20d222e3
Jack Devey <jack@jw3.uk>
2023-01-02 17:55:47 +0000

    Update DeadlineTodoView.swift

--- Deadlines/Todos/DeadlineTodoView.swift
+++ Deadlines/Todos/DeadlineTodoView.swift
@@ -20,6 +20,8 @@ struct DeadlineTodoView: View {
     // The store class functions
     var store = Store()
     
+    @State var isOn1 = false
+    
     // Constructor hack to allow the use
     // of links as state (this is so the
     // list will update)
@@ -29,58 +31,77 @@ struct DeadlineTodoView: View {
     }
     
     var body: some View {
-        
-        List {
-            // Show each todo for the item
-            ForEach(todos.sorted(using: SortDescriptor(\DeadlineTodo.placement))) { link in
-                // Show each link as a link (with label)
-                Text(link.name!)
-            }
-            // When a todo is moved (order has changed)
-            .onMove { from, to in
-                // Move the links around as changed
-                from.map { todos[$0] }.forEach{ link in
-                    todos.move(fromOffsets: from, toOffset: to)
+            
+            List {
+                Section {
+                    HStack {
+                        Image(systemName: "exclamationmark.circle")
+                            .imageScale(.large)
+                            .foregroundColor(.systemYellow)
+                        Text("Partial")
+                        Spacer()
+                        Text("33%")
+                            .monospacedDigit()
+                            .foregroundColor(.secondary)
+                    }
+                    NavigationLink(destination: DeadlineStatusExplainView()) {
+                        Image(systemName: "questionmark.circle")
+                            .imageScale(.large)
+                        Text("About status")
+                    }
+                    .foregroundColor(.secondary)
                 }
-                // Loop through todos and update placement
-                for idx in todos.indices {
-                    todos[idx].placement = Int16(idx)
+                // Show each todo for the item
+                ForEach(todos.sorted(using: SortDescriptor(\DeadlineTodo.placement))) { todo in
+                    // Show each link as a link (with label)
+                    Toggle(todo.name!, isOn: $isOn1)
+                        .toggleStyle(CheckboxToggleStyle())
                 }
-                // Save changes
-                store.save(viewContext: viewContext)
-            }
-            // When a todo has been deleted
-            .onDelete { offsets in
-                // Delete a todo from the deadline
-                withAnimation {
-                    offsets.map { todos[$0] }.forEach(viewContext.delete)
-                    todos.remove(atOffsets: offsets)
-                    store.save(viewContext: viewContext) // Save changes
+                // When a todo is moved (order has changed)
+                .onMove { from, to in
+                    // Move the links around as changed
+                    from.map { todos[$0] }.forEach{ link in
+                        todos.move(fromOffsets: from, toOffset: to)
+                    }
+                    // Loop through todos and update placement
+                    for idx in todos.indices {
+                        todos[idx].placement = Int16(idx)
+                    }
+                    // Save changes
+                    store.save(viewContext: viewContext)
                 }
-            }
-        }
-        .navigationTitle("Checklist")
-        .toolbar {
-            EditButton()
-            // New button
-            Button() {
-                showingNewLink.toggle()
-            } label: {
-                Label("New", systemImage: "plus")
-            }
-        }
-        .sheet(isPresented: $showingNewLink) {
-            NewDeadlineTodoSheet { todo in
-                // Set placement to end
-                todo.placement = Int16(todos.endIndex)
-                // Save changes
-                item.addToTodos(todo)
-                store.save(viewContext: viewContext)
-                // Add todo to todos todo
-                withAnimation {
-                    todos.append(todo)
+                // When a todo has been deleted
+                .onDelete { offsets in
+                    // Delete a todo from the deadline
+                    withAnimation {
+                        offsets.map { todos[$0] }.forEach(viewContext.delete)
+                        todos.remove(atOffsets: offsets)
+                        store.save(viewContext: viewContext) // Save changes
+                    }
                 }
             }
-        }
+            .navigationTitle("Checklist")
+            .toolbar {
+                EditButton()
+                // New button
+                Button() {
+                    showingNewLink.toggle()
+                } label: {
+                    Label("New", systemImage: "plus")
+                }
+            }
+            .sheet(isPresented: $showingNewLink) {
+                NewDeadlineTodoSheet { todo in
+                    // Set placement to end
+                    todo.placement = Int16(todos.endIndex)
+                    // Save changes
+                    item.addToTodos(todo)
+                    store.save(viewContext: viewContext)
+                    // Add todo to todos todo
+                    withAnimation {
+                        todos.append(todo)
+                    }
+                }
+            }
     }
 }
