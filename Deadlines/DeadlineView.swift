b09af5dda61a56881ad72c0c05f8e88d3c9fb008
Jack Devey <jack@jw3.uk>
2023-01-08 03:40:05 +0000

    Woah im changing the other stuff now

--- Deadlines/DeadlineView.swift
+++ Deadlines/DeadlineView.swift
@@ -10,59 +10,132 @@ import SwiftUI
 struct DeadlineView: View {
     
     @StateObject var item: Item
-    @State private var showingStatusPopover = false
-    
+        
     var body: some View {
+        
         List {
             HStack {
-                Text("Due date")
-                Spacer()
-                Text(item.date ?? Date.now, style: .date)
-                    .foregroundColor(.secondary)
+                // Show status icon
+                ZStack {
+                    Circle()
+                        .fill(item.getStatus().getIconColor())
+                        .frame(width: 26, height: 26)
+                    Image(systemName: item.getStatus().getIconName())
+                        .imageScale(.small)
+                        .foregroundColor(.white)
+                }
+                // Text
+                VStack(alignment: .leading) {
+                    Text(item.getStatus().getName())
+                        .bold()
+                    Text(item.getStatus().getDescription())
+                        .foregroundColor(.secondary)
+                }
+                // Add padding to left side
+                .padding([.leading], 5)
             }
-            HStack {
-                Text("Status")
-                Spacer()
-                Image(systemName: item.getStatus().getIconName())
-                    .foregroundColor(item.getStatus().getIconColor())
-                Text(item.getStatus().getName())
-                    .foregroundColor(.secondary)
-            }.onPress {
-                showingStatusPopover.toggle()
+        }
+        // Allow deadline title to be edited
+        .navigationTitle($item.name.toUnwrapped(defaultValue: ""))
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbarRole(.editor)
+
+         // Add toolbar items
+        .toolbar {
+            // Icon and title
+            ToolbarItem(id: "title", placement: .principal) {
+                HStack {
+                    // Show deadline icon
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(item.getColour().gradient)
+                            .frame(width: 25, height: 25)
+                        Image(systemName: item.getIconName())
+                            .imageScale(.small)
+                            .foregroundColor(.white)
+                    }
+                    // Show deadline name
+                    VStack(alignment: .leading) {
+                        Text(item.name!)
+                            .bold()
+                    }
+                    .padding([.leading], 4)
+                }
             }
-            Section {
-                // Todos
-                NavigationLink(destination: DeadlineTodoView(deadline: item)) {
+            // Add title menu options
+            ToolbarTitleMenu {
+                Section {
                     HStack {
-                        Label("Checklist", systemImage: "checklist")
-                        Spacer()
-                        Text(String(item.checklistItemsCompletedPercentage) + "%")
-                            .foregroundColor(.secondary)
-                            .monospacedDigit()
+                        // Show deadline icon
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(item.getColour().gradient)
+                                .frame(width: 25, height: 25)
+                            Image(systemName: item.getIconName())
+                                .imageScale(.small)
+                                .foregroundColor(.white)
+                        }
+                        // Show deadline name
+                        VStack(alignment: .leading) {
+                            Text(item.name!)
+                                .bold()
+                        }
                     }
                 }
-                // Notes
-                NavigationLink(destination: NotesView(item: item)) {
-                    Label("Notes", systemImage: "text.justify.leading")
+                Button("Hi") {
+                    
                 }
-                // Links
-                NavigationLink(destination: DeadlineLinkView(item: item)) {
-                    Label("Links", systemImage: "link")
-                }
-                // Submitted
-                Toggle(isOn: $item.submitted) {
-                    Label("Submitted", systemImage: "paperplane")
-                }
+                // Rename the deadline
+                RenameButton()
             }
         }
-        .navigationTitle(item.name!)
-        .sheet(isPresented: $showingStatusPopover) {
-            VStack(alignment: .leading) {
-                StatusView(status: item.getStatus(), showDescription: true)
-                    .fill(alignment: .leading)
-                    .padding(10)
-                
-            }.presentationDetents([.fraction(0.15)])
-        }
     }
+//            List {
+//                HStack {
+//                    Text("Due date")
+//                    Spacer()
+//                    Text(item.date ?? Date.now, style: .date)
+//                        .foregroundColor(.secondary)
+//                }
+//                HStack {
+//                    Text("Status")
+//                    Spacer()
+//                    Image(systemName: item.getStatus().getIconName())
+//                        .foregroundColor(item.getStatus().getIconColor())
+//                    Text(item.getStatus().getName())
+//                        .foregroundColor(.secondary)
+//                }
+//                Section {
+//                    // Todos
+//                    NavigationLink(destination: DeadlineTodoView(deadline: item)) {
+//                        HStack {
+//                            Label("Checklist", systemImage: "checklist")
+//                            Spacer()
+//                            Text(String(item.checklistItemsCompletedPercentage) + "%")
+//                                .foregroundColor(.secondary)
+//                                .monospacedDigit()
+//                        }
+//                    }
+//                    // Notes
+//                    NavigationLink(destination: NotesView(item: item)) {
+//                        Label("Notes", systemImage: "text.justify.leading")
+//                    }
+//                    // Links
+//                    NavigationLink(destination: DeadlineLinkView(item: item)) {
+//                        Label("Links", systemImage: "link")
+//                    }
+//                    // Submitted
+//                    Toggle(isOn: $item.submitted) {
+//                        Label("Submitted", systemImage: "paperplane")
+//                    }
+//                }
+//            }
+//        }
+    
 }
+
+                         extension Binding {
+                              func toUnwrapped<T>(defaultValue: T) -> Binding<T> where Value == Optional<T>  {
+                                 Binding<T>(get: { self.wrappedValue ?? defaultValue }, set: { self.wrappedValue = $0 })
+                             }
+                         }
